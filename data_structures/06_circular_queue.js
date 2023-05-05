0632d7c1946fb846c61052741c4ea6553c71658a
Francisco D <drz.y2k@gmail.com>
2023-05-05 17:40:12 -0600

    6 Circular Queue

---
+++ data_structures/06_circular_queue.js
@@ -0,0 +1,41 @@
+class CircularQueue {
+	constructor(size) {
+
+		this.queue = [];
+		this.read = 0;
+		this.write = 0;
+		this.max = size - 1;
+
+		while (size > 0) {
+			this.queue.push(null);
+			size--;
+		}
+	}
+
+	print() {
+		return this.queue;
+	}
+
+	enqueue(item) {
+		// Only change code below this line
+		if (this.write > this.max) this.write = 0;
+		if (this.queue[this.write] != null) return null;
+		this.queue[this.write] = item;
+		this.write++;
+		return item;
+
+		// Only change code above this line
+	}
+
+	dequeue() {
+		// Only change code below this line
+		if (this.read > this.max) this.read = 0;
+		if (this.queue[this.read] == null) return null;
+		let val = this.queue[this.read];
+		this.queue[this.read] = null;
+		this.read++;
+		return val;
+
+		// Only change code above this line
+	}
+}
