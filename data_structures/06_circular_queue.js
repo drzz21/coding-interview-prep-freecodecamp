b90248e2c6e7f82187aea163c0156d0cced7ed66
Francisco D <drz.y2k@gmail.com>
2023-05-05 18:03:07 -0600

    --hotfix

--- data_structures/06_circular_queue.js
+++ data_structures/06_circular_queue.js
@@ -20,8 +20,7 @@ class CircularQueue {
 		// Only change code below this line
 		if (this.write > this.max) this.write = 0;
 		if (this.queue[this.write] != null) return null;
-		this.queue[this.write] = item;
-		this.write++;
+		this.queue[this.write++] = item;
 		return item;
 
 		// Only change code above this line
@@ -32,8 +31,7 @@ class CircularQueue {
 		if (this.read > this.max) this.read = 0;
 		if (this.queue[this.read] == null) return null;
 		let val = this.queue[this.read];
-		this.queue[this.read] = null;
-		this.read++;
+		this.queue[this.read++] = null;
 		return val;
 
 		// Only change code above this line
